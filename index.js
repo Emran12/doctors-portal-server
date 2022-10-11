7587bf59be99e89d7f512cbaa3364a38619d6434
Emran12 <emran6452@gmail.com>
2022-10-11 11:11:06 +0600

    server side initialize

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://doctor-admin:<password>@cluster0.kzyfz.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+app.get("/", (req, res) => {
+  res.send("hello from doctors portal");
+});
+
+app.listen(port, () => {
+  console.log(`'app is running at ${port}'`);
+});
